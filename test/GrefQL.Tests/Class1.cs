9c1ec5a0bee4c77179cb485fe89b73041b8dad87
Andrew Peters <anpete@microsoft.com>
2016-03-14 09:52:26 -0700

    File->New Project

---
+++ test/GrefQL.Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GrefQL
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test()
+        {
+            
+        }
+    }
+}
